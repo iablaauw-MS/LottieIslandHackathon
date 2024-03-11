93569630d82fa20143f60fb6df8146e6bf3900ca
Ian Blaauw <iablaauw@microsoft.com>
2024-03-11 12:53:36 -0700

    Added winrt component

---
+++ Samples/Islands/LottieIsland/LottieContentIsland.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "LottieContentIsland.h"
+#include "LottieContentIsland.g.cpp"
+
+namespace winrt::LottieIsland::implementation
+{
+    int32_t LottieContentIsland::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void LottieContentIsland::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ Samples/Islands/LottieIsland/LottieContentIsland.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "LottieContentIsland.g.h"
+
+namespace winrt::LottieIsland::implementation
+{
+    struct LottieContentIsland : LottieContentIslandT<LottieContentIsland>
+    {
+        LottieContentIsland() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::LottieIsland::factory_implementation
+{
+    struct LottieContentIsland : LottieContentIslandT<LottieContentIsland, implementation::LottieContentIsland>
+    {
+    };
+}
