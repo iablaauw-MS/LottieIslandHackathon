b3256dbbfa38aeea2aa3634b67448d9dc48a9258
Ian Blaauw <48028976+iablaauw-MS@users.noreply.github.com>
2024-03-12 11:50:48 -0700

    Get to the red square! (#3)
    * Not compiling yet, but have in theory a bridge and island being created.
    
    * Hook up and show island, with some visual content

--- Samples/Islands/LottieIsland/LottieContentIsland.cpp
+++ Samples/Islands/LottieIsland/LottieContentIsland.cpp
@@ -4,6 +4,16 @@
 
 namespace winrt::LottieIsland::implementation
 {
+    LottieContentIsland::LottieContentIsland(
+        const winrt::Compositor& compositor)
+        : m_compositor(compositor)
+    {
+        m_rootVisual = m_compositor.CreateContainerVisual();
+        m_island = winrt::ContentIsland::Create(m_rootVisual);
+
+        InitializeTree();
+    }
+
     int32_t LottieContentIsland::MyProperty()
     {
         return m_myProperty;
@@ -12,5 +22,41 @@ namespace winrt::LottieIsland::implementation
     void LottieContentIsland::MyProperty(int32_t value)
     {
         m_myProperty = value;
+    }
+
+    void LottieContentIsland::InitializeTree()
+    {
+        // Make a blue square with a red square inside of it.
+        // Add some animations to the red square
+
+        // 300 x 300 blue background
+        auto blueVisual = m_compositor.CreateSpriteVisual();
+        auto blueBrush = m_compositor.CreateColorBrush(winrt::Windows::UI::Colors::Blue());
+        blueVisual.Brush(blueBrush);
+        blueVisual.Size({ 300, 300 });
+
+        m_rootVisual.Children().InsertAtTop(blueVisual);
+
+        // 50 x 50 red square
+        auto redVisual = m_compositor.CreateSpriteVisual();
+        auto redBrush = m_compositor.CreateColorBrush(winrt::Windows::UI::Colors::Red());
+        redVisual.Brush(redBrush);
+        redVisual.Size({ 50, 50 });
+
+        m_rootVisual.Children().InsertAtTop(redVisual);
+
+        // Setup an animation
+
+        auto keyFrameAnimation = m_compositor.CreateVector3KeyFrameAnimation();
+        keyFrameAnimation.InsertKeyFrame(0.0f, { 0, 0, 0 });
+        keyFrameAnimation.InsertKeyFrame(1.0f, { 250.f, 250.f, 0 });
+
+        // Bounce back and forth forever
+        keyFrameAnimation.Duration(2000ms);
+        keyFrameAnimation.Direction(winrt::AnimationDirection::Alternate);
+        keyFrameAnimation.IterationBehavior(winrt::AnimationIterationBehavior::Forever);
+
+        // Start animation
+        redVisual.StartAnimation(L"Offset", keyFrameAnimation);
     }
 }
--- Samples/Islands/LottieIsland/LottieContentIsland.h
+++ Samples/Islands/LottieIsland/LottieContentIsland.h
@@ -6,13 +6,23 @@ namespace winrt::LottieIsland::implementation
 {
     struct LottieContentIsland : LottieContentIslandT<LottieContentIsland>
     {
-        LottieContentIsland() = default;
+        LottieContentIsland(const winrt::Compositor& compositor);
 
         int32_t MyProperty();
         void MyProperty(int32_t value);
 
+        winrt::ContentIsland Island() const
+        {
+            return m_island;
+        }
+
     private:
+        void InitializeTree();
+
         int32_t m_myProperty = 42;
+        winrt::Compositor m_compositor{ nullptr };
+        winrt::ContainerVisual m_rootVisual{ nullptr };
+        winrt::ContentIsland m_island{ nullptr };
     };
 }
 
