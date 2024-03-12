da4dab8fe867b527a939a4cbb99c8e290772f956
getrou <52982368+getrou@users.noreply.github.com>
2024-03-12 13:12:26 -0700

    User/getrou/cswinrt dummy project (#4)
    * Added simple C# version of how to get a Visual out of Lottie. Also tried to wrap Lottie in WinRT but I have failed so far
    
    * Remove WinRT reference from C++ projects until I get it working

---
+++ Samples/Islands/LottieVisualWinRT/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace LottieVisualWinRT
+{
+    // Dummy WinRT object that we are trying to reference from a C++ project
+    public sealed class Class1
+    {
+        public int SampleProperty { get; set; }
+
+        public static void SayHello()
+        {
+            Debug.WriteLine("Hello from your C# WinRT component");
+        }
+    }
+}
