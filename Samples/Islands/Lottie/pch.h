432567fc9aadb07419afc6b7ade5b1ac93a0869c
Ian Blaauw <48028976+iablaauw-MS@users.noreply.github.com>
2024-03-12 13:30:33 -0700

    Remove XAML Stuff (#5)
    * Deleted a bunch of stuff that was XAML or winrt generation only and not needed.
    
    * Deleted more XAML references

--- Samples/Islands/Lottie/pch.h
+++ Samples/Islands/Lottie/pch.h
@@ -7,9 +7,6 @@
 // Windows Header Files
 #include <windows.h>
 
-// This tells App.xaml.g.hpp the app will define its own WinMain
-#define DISABLE_XAML_GENERATED_MAIN
-
 // There's an API named GetCurrentTime in the Storyboard type.
 #undef GetCurrentTime
 
@@ -19,10 +16,7 @@
 // Some generated files, like MainPage.xaml.g.h need files such as Microsoft.UI.Xaml.Markup.h
 // to already be included.  Let's just include a bunch of stuff we know we'll need here in the PCH.
 #include <winrt/Microsoft.UI.Content.h>
+#include <winrt/Microsoft.UI.Composition.h>
 #include <winrt/Microsoft.UI.Dispatching.h>
 #include <winrt/Microsoft.UI.Interop.h>
-#include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
-#include <winrt/Microsoft.UI.Xaml.h>
-#include <winrt/Microsoft.UI.Xaml.Hosting.h>
-#include <winrt/Microsoft.UI.Xaml.Markup.h>
 #include <winrt/Windows.Foundation.h>
