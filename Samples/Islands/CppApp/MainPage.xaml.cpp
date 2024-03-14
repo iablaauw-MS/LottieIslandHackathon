438ef8fbf47e75c3fb32c119f2ebf6b32e01808d
getrou <52982368+getrou@users.noreply.github.com>
2024-03-14 10:20:54 -0700

    Getrou/lottie island winrt (#10)
    * Added CppApp with no Islands to test winrt component'
    
    * C++/WinRT app CppApp is now consuming Lottie through C# Json parser. Still working on porting that functionality to the Island app

---
+++ Samples/Islands/CppApp/MainPage.xaml.cpp
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+#if __has_include("MainPage.g.cpp")
+#include "MainPage.g.cpp"
+#endif
+
+#include <winrt/Microsoft.UI.Xaml.Hosting.h>
+
+namespace winrt
+{
+    using namespace Microsoft::UI::Composition;
+    using namespace Microsoft::UI::Xaml;
+    using namespace Microsoft::UI::Xaml::Controls;
+    using namespace Microsoft::UI::Xaml::Hosting;
+    using namespace Microsoft::UI::Xaml::Media;
+    using namespace Microsoft::UI::Xaml::Media::Animation;
+    using namespace Microsoft::UI::Xaml::Navigation;
+    using namespace Windows::UI::Xaml::Interop;
+}
+
+namespace winrt::CppApp::implementation
+{
+    CppApp::MainPage MainPage::current{ nullptr };
+
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+        MainPage::current = *this;
+        LottieVisualWinRT::Class1::SayHello();
+
+        m_class1 = LottieVisualWinRT::Class1();
+        if (m_class1 != nullptr)
+        {
+            OutputDebugString(L"Meep\n");
+        }
+
+        m_compositor = ElementCompositionPreview::GetElementVisual(MyGrid()).Compositor();
+        m_rootVisual = m_compositor.CreateContainerVisual();
+        ElementCompositionPreview::SetElementChildVisual(MyGrid(), m_rootVisual);
+        m_class1.SetUpLottie(m_compositor, m_rootVisual, L"ms-appx:///LottieLogo1.json");
+    }
+}
